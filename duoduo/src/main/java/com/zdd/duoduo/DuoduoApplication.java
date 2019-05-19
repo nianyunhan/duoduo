26163c43e9e749871dea3dd71f7b6e9710815857
nianyunhan <46374493+nianyunhan@users.noreply.github.com>
2019-05-19 17:03:35 +0800

    原版

---
+++ duoduo/src/main/java/com/zdd/duoduo/DuoduoApplication.java
@@ -0,0 +1,13 @@
+package com.zdd.duoduo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuoduoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuoduoApplication.class, args);
+    }
+
+}
---
+++ duoduo/src/test/java/com/zdd/duoduo/DuoduoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zdd.duoduo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DuoduoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
